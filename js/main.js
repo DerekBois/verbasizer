d1919f6c602468edab53deae64bc254e966d9ff8
Derek Bois <derekabois@gmail.com>
2023-05-08 15:13:53 -0400

    add source templates

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 // These will be included in the main file
 // const will be replaced with var (save every byte)
-import { templates } from "./templates.js";
+import { templates, originals } from "./templates.js";
 import { tags } from "./tags.js";
 
 // console.log(single.match(regex));
 // console.log([...single.matchAll(regex)]);
 // console.log(regex.exec(single));
 
-templates.forEach((template) => {
+templates.forEach((template, i) => {
   const final = template.replaceAll(/<(.*?)>/g, (_, s) => {
     return tags[+s][Math.floor(Math.random() * tags[+s].length)];
   });
-
-  document.querySelector("#output").innerHTML += final + "\n";
+  document.querySelector("#output").innerHTML +=
+    `<strong>${final}</strong>` + "\n";
+  document.querySelector("#output").innerHTML +=
+    originals[i].replace(/</g, "&lt") + "\n\n";
 });
 
 // console.log(single);

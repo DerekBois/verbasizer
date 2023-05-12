104318c651bf7226da3ffad6e3350b8b3da51559
Derek Bois <derekabois@gmail.com>
2023-05-12 17:34:45 -0400

    more updates to templates and tags

--- js/templates.js
+++ js/templates.js
@@ -11,162 +11,35 @@ export const lookup = {
   9: "Intransitive verb",
   10: "Dialogue phrase",
 };
-
-const barbaraAllen = [
-  `<5> she <9>
-To <0> where he <3>
-And when she <8>
-Said, <10>
-`,
-  `<10>
-<9>
-Without <2>
-`,
-  `<10>
-The <0> <2>
-You <8> there
-`,
-  `She <9>, she <9>
-She saw <1> coming
-<9>, <10>
-<8>
-`,
-  `The more she <9>, the more <9>
-Until she <9>
-Sayin', <10>
-<1>
-`,
-  `<8>
-<7>
-<0> <9>
-I <9>
-`,
-  `<8> <0> in <1>
-<8> <0> beside him,
-And <2> <9>
-And <2> <0>
-`,
-  `They <9>
-Till they <7>
-And was there they <3>
-<1>
-`,
-];
-
-const loneGreenValley = [
-  `<4> <2>
-<4>
-There was <1>
-<7> with <1>
-`,
-  `<1> I <3>
-<1> you <3>
-And  <2>
-<1> <8>
-`,
-  `<10>
-<2> are <6> <0>
-While <9> <3>
-<2>
-`,
-  `So <4> they <9>
-<1> I <9>
-<1> we <9>
-With <1> he <8>
-`,
-  `<4> before  him
-She <3>
-But <2>
-He <8> <1>
-`,
-  `<10>
-You know I <3>
-I <3>
-But <2>, <3>
-I <3>
-`,
-];
-
-const omieWise = [
-  `I <3>, I <3>
-I <3> <1>
-<8> <2>
-<8> and <1>
-`,
-  `I <4> <2>
-She <3> nor <1>
-<10>
-<10>
-`,
-  `I <8> and <3>
-<2> where <1>
-<10>
-<10>
-`,
-  `<10>
-<1> and I <3>
-<10>
-<10>
-`,
-  `I <8> he <3>
-She <8> where he <2>
-In<8> and <3>
-As the <1> went <2>
-`,
-  `T'was <1> <3>
-I <3> but she <1>
-<1> <1>
-And she <3>
-`,
-  `They <8> and <8>
-Her <1> they <8>
-Then <8> to <2>
-And <8> <1>
-`,
-  `He <3> but they <8>
-<1> they <3>
-`,
-];
-
-const twentyOneYears = [
-  `<0> said <10>
-You <8> to <1>
-I <3> and she <3>
-<1> is <1>
-`,
-  `I <9> she'll <4>
-To <3> <1>
-<3> <7>
-And I <3> <1>
-`,
-  `<1> <1>
-I <3>,<4>
-He <3> babe and <3>
-<1> <9> <1>
-`,
-  `I <3> <1>
-I <3> and <3>
-If I had <1> where <2>
-Before <1> <4>
-`,
-  `I <8> <1> I <8> <1>
-I <8> <1> I <8> <1>
-I  <8> <1> I <8> <1>
-I <8> <1> of these <1>
-`,
-  `I <3> and <3>
-She <3> I'm <1>
-I <3> I <4> <3> but  <4>
-`,
-  `I <3> with <7>
-Don't <3> <1> you <3>
-Don't <3> <1> <1>
-<1> is a <1>
-`,
-];
 export const templates = [
-  ...barbaraAllen,
-  ...loneGreenValley,
-  ...omieWise,
-  ...twentyOneYears,
+  `<5> she <9>\nTo <0> where he <3>\nAnd when she <8>\nSaid, <10>`,
+  `<10>\nI <9>\n<2>`,
+  `<10>\nThe <0>, <2>\nYou <8> there`,
+  `She <9>, she <9>\nShe saw <1> coming\nShe <9>, <10>\nI <8>`,
+  `He <9>, He <9>\nHe saw <1> coming\nHe <9>, <10>\nI <8>`,
+  `The more she <9>, the more she <9>\nUntil she <9>\nSayin', <10>\n<1>`,
+  `I <8>\n<7>\n<0>, he <9>\nI <9>`,
+  `I <8> <0>, <1>\nI <8> <0> beside him,\nAnd <2> he <9>\nAnd <2> <0>`,
+  `They <9>\nTill they <7>\nAnd it was there they <3>\n<1>`,
+  `<4> <2>\n<4>\nThere was <1>\n<7> with <1>`,
+  `<1> I <3>\n<1> you <3>\nAnd <2>\n<1> I <8>`,
+  `<10>\n<2> are <6> <0>\nWhile I <9>, I <3>\n<2>`,
+  `So <4> they <9>\n<1> I <9>\n<1> we <9>\nWith <1> he <8>`,
+  `<4> before him\nShe <3>\nBut <2>\nHe <8>, <1>`,
+  `<10>\nYou know I <3>\nI <3>\nBut <2>, I <3>\nI <3>`,
+  `I <3>, I <3>\nI <3>, <1>\nThey <8> <2>\nI <8>, <1>`,
+  `<4> <2>\nShe <3>, <1>\n<10>\n<10>`,
+  `I <8> and I <3>\n<2> where <1>\n<10>\n<10>`,
+  `<10>\n<1> and I <3>\n<10>\n<10>`,
+  `I <8> he <3>\nShe <8> <2>\nIn <8> and I <3>\nAs the <1> <2>`,
+  `T'was <1> <3>\nI <3>, <1>\n<1> <1>\nAnd she <3>`,
+  `They <8> and I <8>\nHer <1> they <8>\nThen I <8> <2>\nAnd I <8>, <1>`,
+  `He <3> but they <8>\n<1> they <3>`,
+  `<0> said <10>\nYou <8>, <1>\nI <3> and she <3>\n<1> is <1>`,
+  `I <9> she <4>\nI <3>, <1>\nI <3> <7>\nAnd I <3>, <1>`,
+  `<1> <1>\nI <3>,<4>\nHe <3> babe and I <3>\n<1>, I <9>, <1>`,
+  `I <3>, <1>\nI <3> and I <3>\nIf I had <1> <2>\nBefore <1> <4>`,
+  `I <8>, <1> I <8>, <1>\nI <8>, <1> I <8>, <1>\nI <8>, <1> I <8>, <1>\nI <8>, <1> of these <1>`,
+  `I <3> and I <3>\nShe <3>, <1>\nI <3> <4>, I <3> <4>`,
+  `I <3> with <7>\nDon't I <3>, <1> you <3>\nDon't I <3>, <1> <1>\n<1> is a <1>`,
 ];

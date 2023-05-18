c2bac68b910c6ca06919b0974884030cc641c665
Derek Bois <derekabois@gmail.com>
2023-05-18 13:15:09 -0400

    add contextual and gender tags

--- js/main.js
+++ js/main.js
@@ -1,30 +1,53 @@
 // These will be included in the main file
-// const will be replaced with var (save every byte)
-import { templates, lookup } from "./templates.js";
-import { tags } from "./tags.js";
+import { templates } from "./templates.js";
+import { tags, lookup, genders } from "./tags.js";
 import { Random } from "./randomizer.js";
 
-const R = new Random();
+let R = new Random();
+let rb = +R.random_bool(0.5);
 
-const replaceTags = (temp) =>
-  temp.replaceAll(
-    /<(.*?)>/g,
-    (_, s) =>
-      `<span>${tags[+s][Math.floor(R.random_dec() * tags[+s].length)]}</span>`
-  );
+// Replace the tags that start with < and end with > with the
+// corresponding reference at random
+let replaceTags = (str) => {
+  let rx = /\^\w+\b/g;
 
-const replaceNums = (temp) =>
-  temp.replaceAll(/<(.*?)>/g, (_, s) => `&lt${lookup[+s]}&gt`);
+  const whatever = str
+    .replaceAll(rx, (s) => {
+      let t = s.replace("^", "");
 
-templates.forEach((template, i) => {
-  document.querySelector("#output").innerHTML += `<p><strong>${replaceTags(
-    template
-  )}</strong></p>`;
+      if (/^g\d/.test(t)) return s;
 
+      return `<span>${
+        tags[t][Math.floor(R.random_dec() * tags[t].length)]
+      }</span>`;
+    })
+    .replaceAll(
+      rx,
+      (s) =>
+        `<span>${genders[s.slice(2, 3) - 1][/a/.test(s) ? +rb : +!rb]}</span>`
+    );
+
+  // return whatever
+  //   .split("\n")
+  //   .map((l) => l.charAt(0).toUpperCase() + l.slice(1))
+  //   .join("\n")
+
+  return whatever;
+};
+
+// Replace the numbers used in the template with readable tags
+// for the reference output
+let replaceNums = (temp) =>
+  temp.replaceAll(/\^\w+\b/g, (s) => {
+    let t = s.replace("^", "");
+    if (/^g\d/.test(t)) {
+      let final = /a/.test(t) ? t.replace("a", "f") : t + "m";
+      return `&lt${final}&gt`;
+    }
+    return `&lt${lookup[t]}&gt`;
+  });
+
+templates.forEach((template) => {
   document.querySelector("#output").innerHTML +=
-    replaceNums(template) + "<hr/>";
-  // originals[i].replace(/</g, "&lt") +
-  // "\n\n";
+    `<p>${replaceTags(template)}</p>` + replaceNums(template) + "<hr/>";
 });
-
-// console.log(single);

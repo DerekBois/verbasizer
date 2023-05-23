bfb0db7011f07386364e3f15b9b5565f7a5686f5
Derek Bois <derekabois@gmail.com>
2023-05-22 20:15:28 -0400

    update to add I and you, etc

--- js/main.js
+++ js/main.js
@@ -1,38 +1,54 @@
 // These will be included in the main file
 import { templates } from "./templates.js";
-import { tags, lookup, genders } from "./tags.js";
+import { tags, lookup, genders as g } from "./tags.js";
 import { Random } from "./randomizer.js";
 
 let R = new Random();
-let rb = +R.random_bool(0.5);
 
 // Replace the tags that start with < and end with > with the
 // corresponding reference at random
+// let replaceTags = (str) => {
+//   let rx = /\^\w+\b/g;
+//   let g1 = R.random_bool(0.5);
+//   let g2 = R.random_bool(0.5);
+//   let g3 = R.random_bool(0.5);
+//   g2 = g1 && g2 ? !g2 : g2;
+
+//   return str
+//     .replaceAll(rx, (s) => {
+//       let t = s.replace("^", "");
+//       if (/^g\d/.test(t)) return s;
+
+//       return `<span>${
+//         tags[t][Math.floor(R.random_dec() * tags[t].length)]
+//       }</span>`;
+//     })
+//     .replaceAll(
+//       rx,
+//       (s) =>
+//         `<span>${
+//           genders[/o/.test(s) ? +g2 : +g1][s.slice(2, 3) - 1][+g3]
+//         }</span>`
+//     );
+// };
+
 let replaceTags = (str) => {
   let rx = /\^\w+\b/g;
+  let g1 = R.random_bool(0.5);
+  let g2 = R.random_bool(0.5);
+  let g3 = R.random_bool(0.5);
+  g2 = g1 && g2 ? !g2 : g2;
 
-  const whatever = str
+  return str
     .replaceAll(rx, (s) => {
       let t = s.replace("^", "");
-
       if (/^g\d/.test(t)) return s;
-
-      return `<span>${
-        tags[t][Math.floor(R.random_dec() * tags[t].length)]
-      }</span>`;
+      return tags[t][Math.floor(R.random_dec() * tags[t].length)];
     })
-    .replaceAll(
-      rx,
-      (s) =>
-        `<span>${genders[s.slice(2, 3) - 1][/a/.test(s) ? +rb : +!rb]}</span>`
-    );
-
-  // return whatever
-  //   .split("\n")
-  //   .map((l) => l.charAt(0).toUpperCase() + l.slice(1))
-  //   .join("\n")
-
-  return whatever;
+    .replaceAll(rx, (s) => g[/o/.test(s) ? +g2 : +g1][s.slice(2, 3) - 1][+g3])
+    .split("\n")
+    .map((l) => l.charAt(0).toUpperCase() + l.slice(1))
+    .join("\n");
 };
 
 // Replace the numbers used in the template with readable tags
@@ -41,7 +57,7 @@ let replaceNums = (temp) =>
   temp.replaceAll(/\^\w+\b/g, (s) => {
     let t = s.replace("^", "");
     if (/^g\d/.test(t)) {
-      let final = /a/.test(t) ? t.replace("a", "f") : t + "m";
+      let final = /o/.test(t) ? t : t + "s";
       return `&lt${final}&gt`;
     }
     return `&lt${lookup[t]}&gt`;
